d01496ebfce540165f80d4e1a9aa47429e769b25
William H <william.hermozo@protonmail.com>
2023-01-23 00:16:58 +0000

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://astro-cube.vercel.app/',
+  integrations: [sitemap()],
+});
